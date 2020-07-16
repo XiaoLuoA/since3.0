13d28851fdd14e35dfad9df6c6f781699505df1c
jayzh <jay__zh@163.com>
2020-07-16 17:52:10 +0800

    jayzh 于2020-7-13 15：47提交Since 3.0 删除留言版功能

--- src/main/java/com/since/sincethird/ret/UserResult.java
+++ src/main/java/com/since/sincethird/ret/UserResult.java
@@ -5,4 +5,8 @@ package com.since.sincethird.ret;
  */
 public interface UserResult extends Result {
     Code USER_NOT_FIND = new Code("801_402","该用户不存在！");
+    /**
+     * @author jayzh
+     */
+    Code OPEN_ID_NOT_SAME=new Code("801_403","openid不匹配");
 }
